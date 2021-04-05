6f1cb92073d115aa60f55c1317c168c6323a9bcc
fwqefwqef2 <62837667+fwqefwqef2@users.noreply.github.com>
2021-04-05 16:09:06 +0900

    Update core.ts

--- server/chat-commands/core.ts
+++ server/chat-commands/core.ts
@@ -1284,7 +1284,10 @@ export const commands: ChatCommands = {
 		}
 	},
 
-	acceptbattle(target, room, user) {
+	acceptbattle(target, room, user, connection) {
+		const ready = await this.prepBattle(connection, 'challenge');
+		if (!ready) return false;
+
 		const [roomid, senderID] = target.split(',').map(part => part.trim());
 		const targetRoom = Rooms.get(roomid);
 		if (!targetRoom) return this.errorReply(`Room ${roomid} not found`);
